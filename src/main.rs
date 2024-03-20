61a4001a0bd1ad6805693506a9f78f5eee72d7e9
wattlebirdaz <wattlebirdaz@gmail.com>
2024-03-20 13:30:22 -0500

    improve constructing select

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,40 @@
-use std::{collections::HashSet, rc::Rc};
+use std::collections::HashSet;
 
 enum Expr {
-    ColRef {id: usize},
-    Int {val: i64},
-    Eq {left: Rc<Expr>, right: Rc<Expr>},
+    ColRef { id: usize },
+    Int { val: i64 },
+    Eq { left: Box<Expr>, right: Box<Expr> },
 }
 
 impl Expr {
     fn col_ref(id: usize) -> Expr {
-        Expr::ColRef {id}
+        Expr::ColRef { id }
     }
 
     fn int(val: i64) -> Expr {
-        Expr::Int {val}
+        Expr::Int { val }
     }
 
     fn eq(self, other: Expr) -> Expr {
-        Expr::Eq {left: Rc::new(self), right: Rc::new(other)}
+        Expr::Eq {
+            left: Box::new(self),
+            right: Box::new(other),
+        }
     }
 
     fn print(&self, indent: usize, out: &mut String) {
         match self {
             Expr::ColRef { id } => {
                 out.push_str(&format!("@{}", id));
-            },
+            }
             Expr::Int { val } => {
                 out.push_str(&format!("{}", val));
-            },
+            }
             Expr::Eq { left, right } => {
                 left.print(indent, out);
                 out.push_str("=");
                 right.print(indent, out);
-            },
+            }
         }
     }
 }
@@ -51,13 +54,13 @@ impl Expr {
 impl Expr {
     fn free(&self) -> HashSet<usize> {
         match self {
-            Expr::ColRef {id} => {
+            Expr::ColRef { id } => {
                 let mut set = HashSet::new();
                 set.insert(*id);
                 set
-            },
-            Expr::Int {val: _} => HashSet::new(),
-            Expr::Eq {left, right} => {
+            }
+            Expr::Int { val: _ } => HashSet::new(),
+            Expr::Eq { left, right } => {
                 let mut set = left.free();
                 set.extend(right.free());
                 set
@@ -76,58 +79,72 @@ enum RelExpr {
         column_names: Vec<usize>,
     },
     Select {
-        src: Rc<RelExpr>,
+        src: Box<RelExpr>,
         predicates: Vec<Expr>,
     },
     Join {
-        left: Rc<RelExpr>,
-        right: Rc<RelExpr>,
+        left: Box<RelExpr>,
+        right: Box<RelExpr>,
         predicates: Vec<Expr>,
     },
     Project {
-        src: Rc<RelExpr>,
+        src: Box<RelExpr>,
         cols: HashSet<usize>,
     },
 }
 
 impl RelExpr {
     fn scan(table_name: String, column_names: Vec<usize>) -> RelExpr {
-        RelExpr::Scan {table_name, column_names}
+        RelExpr::Scan {
+            table_name,
+            column_names,
+        }
     }
 
     fn select(self, mut predicates: Vec<Expr>) -> RelExpr {
         if let RelExpr::Select {
             src,
             predicates: mut preds,
-        } = self {
+        } = self
+        {
             preds.append(&mut predicates);
-            return src.select(preds)
+            return src.select(preds);
         }
 
-        RelExpr::Select {src: Rc::new(self), predicates}
+        RelExpr::Select {
+            src: Box::new(self),
+            predicates,
+        }
     }
 
     fn join(self, other: RelExpr, mut predicates: Vec<Expr>) -> RelExpr {
-        // RelExpr::Join {left: Rc::new(self), right: Rc::new(other), predicates}
+        // RelExpr::Join {left: Box::new(self), right: Box::new(other), predicates}
         for i in 0..predicates.len() {
             if predicates[i].bound_by(&self) {
                 // We can push this predicate to the left side
                 let predicate = predicates.swap_remove(i);
-                return self.select(vec![predicate]).join(other, predicates)
+                return self.select(vec![predicate]).join(other, predicates);
             }
 
             if predicates[i].bound_by(&other) {
                 // We can push this predicate to the right side
                 let predicate = predicates.swap_remove(i);
-                return self.join(other, predicates).select(vec![predicate])
+                return self.join(other.select(vec![predicate]), predicates);
             }
         }
 
-        RelExpr::Join {left: Rc::new(self), right: Rc::new(other), predicates}
+        RelExpr::Join {
+            left: Box::new(self),
+            right: Box::new(other),
+            predicates,
+        }
     }
 
     fn project(self, cols: HashSet<usize>) -> RelExpr {
-        RelExpr::Project {src: Rc::new(self), cols}
+        RelExpr::Project {
+            src: Box::new(self),
+            cols,
+        }
     }
 }
 
@@ -143,10 +160,8 @@ impl RelExpr {
     //   * The columns that are bound by evaluating an expression
     fn free(&self) -> HashSet<usize> {
         match self {
-            RelExpr::Scan {..} => {
-                HashSet::new()
-            },
-            RelExpr::Select {src, predicates} => {
+            RelExpr::Scan { .. } => HashSet::new(),
+            RelExpr::Select { src, predicates } => {
                 // For each predicate, identify the free columns.
                 // Take the set difference of the free columns and the src attribute set.
                 let mut set = src.free();
@@ -154,21 +169,27 @@ impl RelExpr {
                     set.extend(pred.free());
                 }
                 set.difference(&src.att()).cloned().collect()
-            },
-            RelExpr::Join {left, right, predicates} => {
+            }
+            RelExpr::Join {
+                left,
+                right,
+                predicates,
+            } => {
                 let mut set = left.free();
                 set.extend(right.free());
                 for pred in predicates {
                     set.extend(pred.free());
                 }
-                set.difference(&left.att().union(&right.att()).cloned().collect()).cloned().collect()
-            },
-            RelExpr::Project {src, cols} => {
+                set.difference(&left.att().union(&right.att()).cloned().collect())
+                    .cloned()
+                    .collect()
+            }
+            RelExpr::Project { src, cols } => {
                 // We assume that the cols are in the src attribute set.
                 // Otherwise we can't project the columns.
                 // Therefore, we don't need to check the free set of the cols.
                 src.free()
-            },
+            }
         }
     }
 
@@ -181,39 +202,35 @@ impl RelExpr {
     // * "att" is the set of columns that we have (the column names of the result of RelExpr)
     fn att(&self) -> HashSet<usize> {
         match self {
-            RelExpr::Scan {table_name: _, column_names} => {
-                column_names.iter().cloned().collect()
-            },
-            RelExpr::Select {src, ..} => src.att(),
-            RelExpr::Join {left, right, ..} => {
+            RelExpr::Scan {
+                table_name: _,
+                column_names,
+            } => column_names.iter().cloned().collect(),
+            RelExpr::Select { src, .. } => src.att(),
+            RelExpr::Join { left, right, .. } => {
                 let mut set = left.att();
                 set.extend(right.att());
                 set
-            },
-            RelExpr::Project {cols, ..} => cols.clone(),
+            }
+            RelExpr::Project { cols, .. } => cols.clone(),
         }
     }
-
-
-
 }
 
 impl RelExpr {
     fn print(&self, indent: usize, out: &mut String) {
         match self {
-            RelExpr::Scan {table_name, column_names} => {
-                out.push_str(&format!(
-                    "{}-> scan({:?}, {:?})\n",
-                    " ".repeat(indent),
-                    table_name,
-                    column_names
-                ))
-            }
-            RelExpr::Select {src, predicates} => {
-                out.push_str(&format!(
-                    "{}-> select(\n",
-                    " ".repeat(indent)
-                ));
+            RelExpr::Scan {
+                table_name,
+                column_names,
+            } => out.push_str(&format!(
+                "{}-> scan({:?}, {:?})\n",
+                " ".repeat(indent),
+                table_name,
+                column_names
+            )),
+            RelExpr::Select { src, predicates } => {
+                out.push_str(&format!("{}-> select(", " ".repeat(indent)));
                 let mut split = "";
                 for pred in predicates {
                     out.push_str(split);
@@ -222,11 +239,13 @@ impl RelExpr {
                 }
                 out.push_str(")\n");
                 src.print(indent + 2, out);
-            },
-            RelExpr::Join {left, right, predicates} => {
-                out.push_str(&format!(
-                    "{}-> join(", " ".repeat(indent)
-                ));
+            }
+            RelExpr::Join {
+                left,
+                right,
+                predicates,
+            } => {
+                out.push_str(&format!("{}-> join(", " ".repeat(indent)));
                 let mut split = "";
                 for pred in predicates {
                     out.push_str(split);
@@ -236,19 +255,14 @@ impl RelExpr {
                 out.push_str(")\n");
                 left.print(indent + 2, out);
                 right.print(indent + 2, out);
-            },
-            RelExpr::Project {src, cols} => {
-                out.push_str(&format!(
-                    "{}-> project({:?})\n",
-                    " ".repeat(indent),
-                    cols
-                ));
+            }
+            RelExpr::Project { src, cols } => {
+                out.push_str(&format!("{}-> project({:?})\n", " ".repeat(indent), cols));
                 src.print(indent + 2, out);
-            },
+            }
         }
     }
 }
-
 
 fn main() {
     let left = RelExpr::scan("a".into(), vec![0, 1]);
@@ -259,7 +273,8 @@ fn main() {
         vec![
             Expr::col_ref(0).eq(Expr::col_ref(2)),
             Expr::col_ref(1).eq(Expr::int(100)),
-        ]
+            Expr::col_ref(2).eq(Expr::int(200)),
+        ],
     );
 
     let mut out = String::new();
